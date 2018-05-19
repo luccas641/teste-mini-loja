394e0049250350b661ce3c767011eee771c13199
Luccas Almeida <luccas@vpeventos.com>
2018-05-19 10:27:13 -0300

    Listagem de categorias inicial via API

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Category;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+       /**
+     * Return a paginated list of categories.
+     *
+     * @return CategoryResource
+     */
+    public function index()
+    {
+        $categories = Category::latest()
+            ->paginate(20);
+
+        return CategoryResource::collection($categories); 
+    }
+
+    /**
+     * Fetch and return the category.
+     *
+     * @param Category $category
+     * @return CategoryResource
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Validate and save a new category to the database.
+     *
+     * @param Request $request
+     * @return CategoryResource
+     */
+    public function store(Request $request)
+    {
+        $category = $this->validate($request, [
+            'name' => 'required|max:50',
+        ]);
+
+        $category = Category::create($category);
+
+        return new CategoryResource($category);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::resource('categories', 'Api\CategoryController')
+    ->only(['index', 'show']);
+
 Route::resource('products', 'Api\ProductController')
     ->only(['index', 'show']);
