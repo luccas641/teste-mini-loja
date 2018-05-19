73b2357f84b340f1f229caf904a9f936076d0d4c
Luccas Almeida <luccas@vpeventos.com>
2018-05-19 10:21:45 -0300

    Listagem de produtos inicial via API

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Product;
+use App\Http\Resources\ProductResource;
+
+class ProductController extends Controller
+{
+       /**
+     * Return a paginated list of products.
+     *
+     * @return ProductResource
+     */
+    public function index()
+    {
+        $products = Product::latest()
+            ->paginate(20);
+
+        return ProductResource::collection($products);
+    }
+
+    /**
+     * Fetch and return the product.
+     *
+     * @param Product $product
+     * @return ProductResource
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Validate and save a new product to the database.
+     *
+     * @param Request $request
+     * @return ProductResource
+     */
+    public function store(Request $request)
+    {
+        $product = $this->validate($request, [
+            'name' => 'required|max:50',
+            'price' => 'required|numeric|min:0',
+            'description' => 'required'
+        ]);
+
+        $product = Product::create($product);
+
+        return new ProductResource($product);
+    }
+}
